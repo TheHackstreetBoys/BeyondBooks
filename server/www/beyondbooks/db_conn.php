e13d02f2cea950b9fb8a0d700b62d247b43090de
Harish Krupo <harishkrupo@gmail.com>
2015-10-20 20:10:13 +0530

    added core functionality to server

--- server/www/beyondbooks/db_conn.php
+++ server/www/beyondbooks/db_conn.php
@@ -1,11 +1,37 @@
 <?php
 
 $dbconn=null;
-
+$useragent=$_SERVER['HTTP_USER_AGENT'];
 function connect_db()
 {
 	global $dbconn;
 	$dbconn=pg_connect("host=localhost dbname=TITVBB user=postgres password=random") or die("couldnot connect!!!");
 }
-
+function sql_query($str)
+{
+	global $dbconn,$useragent;
+	echo $str;
+	$result=pg_query($dbconn,$str);
+	
+	if (!$result) {
+		echo "An error occurred.\n".pg_last_error();
+		exit;
+	}
+	if($useragent!="maakichut")
+	{
+		while ($row = pg_fetch_row($result)) {
+			var_dump($row);
+			echo "<br />\n";
+		}
+	}
+	else 
+	{
+		$jobj=array();
+		while ($row = pg_fetch_row($result)) {
+			array_push($jobj, $row);
+		}
+		echo json_encode($jobj);
+	}
+	
+}
 ?>
