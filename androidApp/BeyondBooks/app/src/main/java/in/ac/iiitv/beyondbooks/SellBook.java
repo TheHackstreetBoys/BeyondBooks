b1d0697f4ec14a1496f172c578ee33c4cd2683fe
Harish Krupo <harishkrupo@gmail.com>
2015-10-22 23:46:53 +0530

    started

---
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/SellBook.java
@@ -0,0 +1,13 @@
+package in.ac.iiitv.beyondbooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SellBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_book);
+    }
+}
