8d5bc596e6dbbb8bf2445058d9ca0509603f9461
Harish Krupo <harishkrupo@gmail.com>
2015-10-23 01:32:04 +0530

    UI created

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/MainActivity.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/MainActivity.java
@@ -1,5 +1,6 @@
 package in.ac.iiitv.beyondbooks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Intent intent = new Intent(this,SellBook.class);
+        startActivity(intent);
 
     }
 
