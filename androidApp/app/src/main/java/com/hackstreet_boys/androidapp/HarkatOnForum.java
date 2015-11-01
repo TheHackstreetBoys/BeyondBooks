c5dc55442e4336a04cc20a1c91a4e61e9d349cd9
Gaurav Yadav <gauravshelangia@gmail.com>
2015-11-01 12:06:33 +0530

    created the wireframe8, wireframe12(Harkatz on forum), wireframe13(activity on forum ) and wireframe20

---
+++ androidApp/app/src/main/java/com/hackstreet_boys/androidapp/HarkatOnForum.java
@@ -0,0 +1,13 @@
+package com.hackstreet_boys.androidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HarkatOnForum extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_harkat_on_forum);
+    }
+}
---
+++ androidApp/app/src/main/java/com/hackstreet_boys/androidapp/wireframe20.java
@@ -0,0 +1,13 @@
+package com.hackstreet_boys.androidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class wireframe20 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wireframe20);
+    }
+}
