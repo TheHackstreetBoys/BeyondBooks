caf0f098917d308eaab5009811c8c670489cc9ff
Gaurav Yadav <gauravshelangia@gmail.com>
2015-11-08 15:52:01 +0530

    UI for wireframe 8, 12, 13, 20 has been completed

---
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/wireframe13.java
@@ -0,0 +1,13 @@
+package in.ac.iiitv.beyondbooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class wireframe13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wireframe13);
+    }
+}
