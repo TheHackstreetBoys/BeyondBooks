2eac4c11fd1c8d3d396032d97833b808596afff1
ANJUL TYAGI <anjul.ten@gmail.com>
2015-11-13 15:22:17 +0530

    Changed the get_activities method of RequestServer.java

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Comments.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Comments.java
@@ -1,23 +1,38 @@
 package in.ac.iiitv.beyondbooks;
 
+
 /**
  * Created by anjul on 11/11/15.
  */
 public class Comments {
-    private String name; // Name of the user
+    private Integer user_id; // Name of the user
     private String text;
-    Comments(String name, String text){
-        this.name = name;
-        this.text = text;
+    private Integer comment_id;
+
+    public Integer getComment_id() {
+        return comment_id;
     }
-    public String getName(){
-        return name;
+
+    public void setComment_id(Integer comment_id) {
+        this.comment_id = comment_id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    Comments(Integer user_id, String text, Integer comment_id){
+        this.user_id = user_id;
+        this.text = text;
+        this.comment_id = comment_id;
+
     }
     public String getText(){
         return text;
-    }
-    public void  setName(String name){
-        this.name = name;
     }
     public void setText(String text){
         this.text = text;
