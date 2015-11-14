60f6161934e1839f153bf749ee6f4d25ca5cedea
ANJUL TYAGI <anjul.ten@gmail.com>
2015-11-14 15:25:06 +0530

    Added a few more functionalities

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/HarkatOnForum.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/HarkatOnForum.java
@@ -1,6 +1,7 @@
 package in.ac.iiitv.beyondbooks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class HarkatOnForum extends Activity {
@@ -9,5 +10,9 @@ public class HarkatOnForum extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_harkat_on_forum);
+        Intent intent = getIntent();
+        UserData userData = (UserData) intent.getSerializableExtra("user_data");
+        RequestServer requestServer = new RequestServer();
+        ForumActivities forumActivities = requestServer.get_forum_activities(userData.getId());
     }
 }
