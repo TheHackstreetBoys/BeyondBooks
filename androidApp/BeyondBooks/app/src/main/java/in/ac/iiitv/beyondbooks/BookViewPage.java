1e8709058aeec7a8771e894ee83d83cf5926fe73
Gaurav Yadav <gauravshelangia@gmail.com>
2015-11-14 06:57:01 +0530

    frame 6 removed

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/BookViewPage.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/BookViewPage.java
@@ -1,6 +1,6 @@
 package in.ac.iiitv.beyondbooks;
 
-import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,30 +22,42 @@ public class BookViewPage extends AppCompatActivity {
     ArrayList<LinearLayout> topratedbooks,newaddedbooks;
     ArrayAdapter<LinearLayout> toprated,newadded;
     LinearLayout topbook,newbook;
+    TextView tv1;
+    ImageView iv1;
+    RatingBar rb1;
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_view_page);
 
-        ImageView iv1 = new ImageView(this);
-        iv1.setImageDrawable(getDrawable(R.mipmap.ic_launcher));
-        TextView tv1 = new TextView(this);
+        //set image
+        iv1 = (ImageView) findViewById(R.id.image_newbook);
+        iv1.setImageDrawable(getResources().getDrawable(R.drawable.user_image));
+
+        tv1 = (TextView) findViewById(R.id.newbookname);
         tv1.setText("Disrete math");
-        RatingBar rb1 = new RatingBar(this);
+
+        rb1 = (RatingBar) findViewById(R.id.ratingsnew);
         rb1.setRating(3);
 
+
         topbook = (LinearLayout) findViewById(R.id.linear_topbook);
-        //topbook.setLayoutParams(new layout);
-        topbook.addView(iv1);
 
-        hsv.addView(iv1);
+        LinearLayout layout_temp = new LinearLayout(this);
+        layout_temp.addView(iv1);
+        layout_temp.addView(tv1);
+        layout_temp.addView(rb1);
 
+        topbook.addView(layout_temp);
+
+        hsv.addView(topbook);
+        /*
         hsv1 = (HorizontalScrollView)findViewById(R.id.toprated);
         ImageView iv2 = new ImageView(this);
         iv1.setImageDrawable(getDrawable(R.mipmap.ic_launcher));
         hsv1.addView(iv2);
+        */
 
 
     }
