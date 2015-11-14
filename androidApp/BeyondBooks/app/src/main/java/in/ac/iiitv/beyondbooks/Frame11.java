f4e7e6ea5eccb2a7bdd3c3f7f29c94f4d80ae532
ANJUL TYAGI <anjul.ten@gmail.com>
2015-11-14 18:18:00 +0530

    Added a few more functionalities

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Frame11.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Frame11.java
@@ -1,13 +1,15 @@
 package in.ac.iiitv.beyondbooks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Frame11 extends AppCompatActivity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame11);
+
     }
 }
