10e44f006cfe98a85b8c131411745bde2b9e92d7
ANJUL TYAGI <anjul.ten@gmail.com>
2015-11-14 12:34:41 +0530

    Added functionality to Frame5.java

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Frame5.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Frame5.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Frame5 extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class Frame5 extends AppCompatActivity {
+    SearchOutputReturn searchOutputReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame5);
         Intent intent = getIntent();
+        searchOutputReturn = (SearchOutputReturn) intent.getSerializableExtra("search_results");
     }
 }
