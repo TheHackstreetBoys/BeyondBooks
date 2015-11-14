4e3ed4e443cef79087599a3a3281f259582f0859
Harish Krupo <harishkrupo@gmail.com>
2015-11-14 17:48:06 +0530

    get_notification change

--- server/www/beyondbooks/andy_get_notification.php
+++ server/www/beyondbooks/andy_get_notification.php
@@ -1,11 +1,12 @@
 <?php
 require 'db_conn.php';
 $username = $_POST['user_id'];
-
+$username = "201351010";
 $query = "select * from notify where whom='$username';";
 $result = pg_query($query);
 $res_arr = array();
-while (row = pg_fetch_array($result))
+
+while ($row = pg_fetch_array($result))
 {
     array_push($res_arr,$row['des']);
 }
