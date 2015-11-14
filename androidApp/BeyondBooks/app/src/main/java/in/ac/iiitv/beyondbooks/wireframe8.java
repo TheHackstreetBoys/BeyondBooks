3285a414f30dae67192a2b7279f07f4cdd908365
Gaurav Yadav <gauravshelangia@gmail.com>
2015-11-14 09:58:03 +0530

    some functionality added to the frame8/user_profile

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/wireframe8.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/wireframe8.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class wireframe8 extends AppCompatActivity {
 
+    ImageView user_image;
     ListView notification_list ;
+    TextView username,userid;
+    ArrayList<String> notifications;
     ArrayAdapter<String> adapter_notification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,18 @@ public class wireframe8 extends AppCompatActivity {
         userData.setUploads(temp2.getUploads());
         //Till here... userData contains every information about the user. Use it to populate the page.
 
+        //set image of the user
+        user_image = (ImageView) findViewById(R.id.user_image);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put(userData.getImage_link(), R.drawable.user_image);
+        user_image.setImageResource(map.get(userData.getImage_link()));
+
+        //set username
+        username = (TextView) findViewById(R.id.user_name);
+        username.setText(userData.getUser_name());
+        //set user id
+        userid = (TextView) findViewById(R.id.user_id);
+        userid.setText(userData.getId());
 
 
         notification_list = (ListView) findViewById(R.id.list_notification);
