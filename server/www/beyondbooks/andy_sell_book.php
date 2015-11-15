01af4402235e670b7f544c3e440c2815111d2ec4
Harish Krupo <harishkrupo@gmail.com>
2015-11-15 23:48:30 +0530

    heavy change identify_book

--- server/www/beyondbooks/andy_sell_book.php
+++ server/www/beyondbooks/andy_sell_book.php
@@ -6,25 +6,6 @@ $age=$_POST['age'];
 $price=$_POST['price'];
 $description = $_POST['description'];
 
-if(pg_num_rows(pg_query("select * from books where isbn=$isbn")) == 0)
-{
-    require 'bookdetails.php';
-    $result = getDetails($isbn);
-
-    $title = $result['title'];
-    $description = $result['description'];
-    $publisher = $result['publisher'];
-    $query = "insert into books (isbn, title, publisher,description, by_user, ts) values ('$isbn', '$title',
-                                    '$publisher', '$description', '$username', CURRENT_TIMESTAMP);";
-    $imagelink= $result['image-link'];
-
-    $content = file_get_contents($imagelink);
-    $fp = fopen("../books_pics/$isbn.jpg", "w");
-    fwrite($fp, $content);
-    fclose($fp);
-    $res1 = pg_query($query);
-
-}
 $query = "insert into pbase (ts,sellerid ) values (CURRENT_TIMESTAMP, '$username');select lastval();";
 $result = pg_query($query);
 $row = pg_fetch_array($result);
