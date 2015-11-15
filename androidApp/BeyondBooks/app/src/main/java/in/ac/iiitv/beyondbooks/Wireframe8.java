58543a3b29b3af18d20f360079b5c30c5c25c58e
ANJUL TYAGI <anjul.ten@gmail.com>
2015-11-16 03:49:17 +0530

    added a few more functionalities

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Wireframe8.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/Wireframe8.java
@@ -21,8 +21,6 @@ import java.util.Map;
 
 public class Wireframe8 extends AppCompatActivity {
     private ArrayList<String> notifications;
-
-
     private Button uploadimage;
     ImageView user_image;
     ListView notification_list ;
@@ -34,15 +32,13 @@ public class Wireframe8 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("Reaching here ...");
         setContentView(R.layout.wireframe8);
-        System.out.println("Reaching here ...");
         //Added by Anjul Tyagi
         notifications = new ArrayList<String>();
         RequestServer requestServer = new RequestServer();
         Intent intent = getIntent();
 //        changepass = (TextView) findViewById(R.id.changepass);
-//        userData = (UserData)intent.getSerializableExtra("user_data");
+        userData = (UserData)intent.getSerializableExtra("user_data");
 //        userData.setForumActivities(requestServer.get_forum_activities(userData.getId()));
 //        UserData temp = requestServer.get_user_name_image(userData.getId());
 //        userData.setImage_link(temp.getImage_link());
@@ -57,15 +53,13 @@ public class Wireframe8 extends AppCompatActivity {
         //set image of the user
         //TODO changes to set the image of the user
         user_image = (ImageView) findViewById(R.id.user_image);
-        user_image.setImageBitmap(requestServer.getImage(userData.getId().toString()+"_dp.jpg"));
-
+        user_image.setImageBitmap(requestServer.getImage(userData.getId().toString() + "_dp.jpg"));
         //set username
         username = (TextView) findViewById(R.id.user_name);
         username.setText(userData.getUser_name());
         //set user id
         userid = (TextView) findViewById(R.id.user_id);
-        userid.setText(userData.getId());
-
+        userid.setText(userData.getId().toString());
         //change password method
 //        changepass = (TextView) findViewById(R.id.changepass);
 //        changepass.setOnClickListener(new View.OnClickListener() {
@@ -91,6 +85,7 @@ public class Wireframe8 extends AppCompatActivity {
         //set notifications (Already there is a arraylist for arrayadapter)
         notification_list = (ListView) findViewById(R.id.notification_list);
         adapter_notification = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1,notifications);
+        System.out.println("Reaching here ....");
         notification_list.setAdapter(adapter_notification);
     }
     public void set_dp(View v){
