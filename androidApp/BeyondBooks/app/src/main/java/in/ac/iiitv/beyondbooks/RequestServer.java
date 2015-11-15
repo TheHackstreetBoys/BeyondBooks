0a27902e8e3d3245f3a5fac8e9f74be07463f7dd
Gaurav Yadav <gauravshelangia@gmail.com>
2015-11-16 01:54:14 +0530

    wireframe21 modified and functionality added

--- androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/RequestServer.java
+++ androidApp/BeyondBooks/app/src/main/java/in/ac/iiitv/beyondbooks/RequestServer.java
@@ -2,6 +2,7 @@ package in.ac.iiitv.beyondbooks;
 
 //use this http://stackoverflow.com/questions/9767952/how-to-add-parameters-to-httpurlconnection-using-post
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -706,6 +707,7 @@ public class RequestServer {
         }
         return false;
     }
+
 
     private class Setup extends AsyncTask<ArrayList<Pair<String, String>>, Void, String> {
         HttpURLConnection urlConnection;
