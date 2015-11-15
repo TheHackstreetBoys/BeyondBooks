b18bba20610e839947412153850fd9b8243cdc86
Harish Krupo <harishkrupo@gmail.com>
2015-11-16 02:27:35 +0530

    search changed

--- server/www/beyondbooks/andy_search.php
+++ server/www/beyondbooks/andy_search.php
@@ -3,30 +3,30 @@
 include 'db_conn.php';
 
 $qs = $_POST['query'];
-$query = "select * from (question_forum natural join user_profile) as lauda where lauda.title like '%qs%';";
+$query = "select * from (((question_forum natural join user_profile) as lauda natural join qtags) as chut natural join faculty_tags) as pooku where title like '%$qs%' or htag like '%$qs%' or uid like '%$qs%';";
 $result = pg_query($query);
 
 $for_ret=array();
 while ($row = pg_fetch_array($result)) {
-    append($for_ret,array("qid"=>$row['qid'], "author_id"=>$row['asker'], "author"=> $row['f_name'], "title"=>$row['title']));
+    array_push($for_ret,array("qid"=>$row['qid'], "author_id"=>$row['asker'], "author"=> $row['f_name'], "title"=>$row['title']));
 }
 
 
-$query = "select * from ((review natural join books) as lund natural join rating) as lauda where lauda.title like '%qs%';";
+$query = "select * from ((review natural join books) as lund natural join rating) as lauda where lauda.title like '%$qs%';";
 $result = pg_query($query);
 
 $review_ret=array();
 while ($row = pg_fetch_array($result)) {
-    append($review_ret,array("image_link"=>$row['image_link'], "book_name"=>$row['title'],
+    array_push($review_ret,array("image_link"=>$row['image_link'], "book_name"=>$row['title'],
     "ratings"=> $row['rating'], "isbn"=>$row['isbn']));
 }
 // needed book_name image_link isbn ratings
-$query = "select * from ((pbase natural join single_sell) as lund natural join books) as lauda where lauda.title like '%qs%';";
+$query = "select * from ((pbase natural join single_sell) as lund natural join books) as lauda where lauda.title like '%$qs%';";
 $result = pg_query($query);
 
 $bs_ret=array();
 while ($row = pg_fetch_array($result)) {
-    append($bs_ret,array("image_link"=>$row['image_link'], "book_name"=>$row['title'],
+    array_push($bs_ret,array("image_link"=>$row['image_link'], "book_name"=>$row['title'],
     "ratings"=> $row['rating'], "isbn"=>$row['isbn']));
 }
 
