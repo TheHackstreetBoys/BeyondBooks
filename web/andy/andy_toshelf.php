2e4ad6b09f08ae4fd1148baec2390bba28271d47
akhilesh <akhilkr.delhi@gmail.com>
2015-11-18 17:34:59 +0530

    File renamed

---
+++ web/andy/andy_toshelf.php
@@ -0,0 +1,26 @@
+<?php
+require 'db_conn.php';
+$username = $_POST['user_id'];
+$isbn = $_POST['isbn'];
+$add = $_POST['add'];
+
+if($add=='true')
+{
+
+    $query = "insert into book_shelf values ('$username','$isbn');";
+    $result = $pg_query($query);
+}
+if($add=='false')
+{
+    $query = "delete from book_shelf where user_id='$username' and isbn='$isbn';";
+    $result = $pg_query($query);
+}
+if($result)
+{
+  echo json_encode(array('done_query'=>'true'));
+}
+else
+{
+  echo json_encode(array('done_query'=>'false'));
+}
+?>
